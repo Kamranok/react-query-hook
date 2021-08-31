b663fb04ea22eb9ccb49ce1fbb561aaa02c67282
Kamran Muhammad <kamranmuhammad@Kamrans-Mac-mini.local>
2021-08-31 14:33:06 -0500

    Updates

---
+++ precache-manifest.a705191a2d4584d45bfcb6e0b96f09fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ade49c3e6334f0b09efc50f5ca2d119",
+    "url": "/react-query-hook/index.html"
+  },
+  {
+    "revision": "ec643c2fa6cee242673a",
+    "url": "/react-query-hook/static/css/main.be5e301a.chunk.css"
+  },
+  {
+    "revision": "096fef0983cc882711cf",
+    "url": "/react-query-hook/static/js/2.3182fc6f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-query-hook/static/js/2.3182fc6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec643c2fa6cee242673a",
+    "url": "/react-query-hook/static/js/main.818ec75a.chunk.js"
+  },
+  {
+    "revision": "05a17131f2f3e59a8dc8",
+    "url": "/react-query-hook/static/js/runtime-main.60be81dc.js"
+  }
+]);
